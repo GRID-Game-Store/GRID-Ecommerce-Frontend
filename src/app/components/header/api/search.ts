ce1ff5217658277e8aed94ed01e4e4ac9fb68f7b
Kirill Serebrykov <kirill.baza22@gmail.com>
2024-05-03 01:46:56 +0200

    fix name public url

--- src/app/components/header/api/search.ts
+++ src/app/components/header/api/search.ts
@@ -1,6 +1,6 @@
 export const getGamesByTitle = async (value: string = "") => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_URL}games/search?title=${value}&qty=3`,
+    `${process.env.NEXT_PUBLIC_URL_CLIENT}games/search?title=${value}&qty=3`,
   );
   const game = res.json();
   return game;
--- src/app/components/main/recommendations/api/getGames.tsx
+++ src/app/components/main/recommendations/api/getGames.tsx
@@ -1,7 +1,7 @@
 
 export const getGamesOfferByTab = async (tab: string) => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_URL}games/offers?query=${tab}&qty=5`,
+    `${process.env.NEXT_PUBLIC_URL_CLIENT}games/offers?query=${tab}&qty=5`,
   );
   const game = res.json();
   return game;
@@ -11,18 +11,18 @@ export const getAllGamesBySorting = async (
   search: string,
 ) => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_URL}games?page=${pageParam}&size=23&${search}`,
+    `${process.env.NEXT_PUBLIC_URL_CLIENT}games?page=${pageParam}&size=23&${search}`,
   );
   const game = res.json();
   return game;
 };
 export const getAllFilters = async (filterBy: string) => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}${filterBy}`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL_CLIENT}${filterBy}`);
   const game = res.json();
   return game;
 };
 export const getGameFullInfo = async (activeHover: number) => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}games/${activeHover}`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL_CLIENT}games/${activeHover}`);
   const game = res.json();
   return game;
 };
